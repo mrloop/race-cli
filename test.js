fa816c6b43d56d5c60b90acb309298b7120cde09
mrloop <ewan@mrloop.com>
2017-09-13 20:51:54 +0100

    longer timeout for travis build

--- test.js
+++ test.js
@@ -8,11 +8,11 @@ const cliPath = __dirname + '/index.js'
 process.env.test = true;
 
 test('selectEvent', async t => {
-  const result = await(run(cliPath, ['CCH Junior Road Race', ENTER]));
+  const result = await(run(cliPath, ['CCH Junior Road Race', ENTER], 4000));
   t.regex(result, new RegExp('CCH Junior', 'g'));
 });
 
 test('selectRace', async t => {
-  const result = await(run(cliPath, ['CCH Junior Road Race', ENTER, ENTER], 400));
+  const result = await(run(cliPath, ['CCH Junior Road Race', ENTER, ENTER], 8000));
   t.regex(result, new RegExp('Craig Adams', 'g'));
 });
