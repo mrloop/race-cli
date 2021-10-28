b0da0333a33c99a55593771cb6b5c00e45a485b6
mrloop <ewan@mrloop.com>
2021-10-28 20:48:04 +0100

    Replace ava with qunit
    Less dependencies

--- test.js
+++ test.js
@@ -1,18 +1,20 @@
-'use strict';
+import QUnit from 'qunit';
+import run, { ENTER } from 'inquirer-test';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import test from 'ava';
-import run, { UP, DOWN, ENTER } from 'inquirer-test';
-
+const { test } = QUnit;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const cliPath = __dirname + '/index.js'
 
 process.env.test = true;
 
-test('selectEvent', async t => {
-  const result = await(run([cliPath], ['CCH Junior Road Race', ENTER], 4000));
-  t.regex(result, new RegExp('CCH Junior', 'g'));
+test('selectEvent', async assert => {
+  const result = await run([cliPath], ['CCH Junior Road Race', ENTER], 4000);
+  assert.true(new RegExp('CCH Junior', 'g').test(result));
 });
 
-test('selectRace', async t => {
-  const result = await(run([cliPath], ['CCH Junior Road Race', ENTER, ENTER], 8000));
-  t.regex(result, new RegExp('Craig Adams', 'g'));
+test('selectRace', async assert => {
+  const result = await run([cliPath], ['CCH Junior Road Race', ENTER, ENTER], 8000);
+  assert.true(new RegExp('Craig Adams', 'g').test(result));
 });
