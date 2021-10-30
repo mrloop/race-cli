9e9cf9695504b8604056b82a04279689ce116cdc
mrloop <ewan@mrloop.com>
2021-10-30 10:34:33 +0100

    Reduce fetch delay for more responsive display
    Seems to work alright and not getting rate limited

--- test.js
+++ test.js
@@ -10,11 +10,11 @@ const cliPath = __dirname + '/index.js'
 process.env.test = true;
 
 test('selectEvent', async assert => {
-  const result = await run([cliPath], ['CCH Junior Road Race', ENTER], 4000);
+  const result = await run([cliPath], ['CCH Junior Road Race', ENTER], 1000);
   assert.true(new RegExp('CCH Junior', 'g').test(result), result);
 });
 
 test('selectRace', async assert => {
-  const result = await run([cliPath], ['CCH Junior Road Race', ENTER, ENTER], 8000);
+  const result = await run([cliPath], ['CCH Junior Road Race', ENTER, ENTER], 1000);
   assert.true(new RegExp('Craig Adams', 'g').test(result), result);
 });
